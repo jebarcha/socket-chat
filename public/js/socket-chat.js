a49c360973f041f008b27db789cdea895d36df24
Jebarcha <jebarcha@hotmail.com>
2019-06-26 13:35:24 -0500

    Chat finalizado

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -14,8 +14,10 @@ var usuario = {
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
+
     socket.emit('entrarChat', usuario, function(resp) {
         console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp); // la resp es un arreglo de usuario
     });
 });
 
@@ -25,27 +27,28 @@ socket.on('disconnect', function() {
 });
 
 // Enviar información
-// socket.emit(
-//     'crearMensaje', {
-//         usuario: 'Fernando',
-//         mensaje: 'Hola Mundo'
-//     },
-//     function(resp) {
-//         console.log('respuesta server: ', resp);
-//     }
-// );
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-    console.log('Servidor:', mensaje);
+    // console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-// Escuchar cuando un usuario entra o sale del chat
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersonas', function(personas) {
-    console.log(personas);
+    console.log('llamar al listaPersona ', personas);
+    renderizarUsuarios(personas); // personas es un arreglo de usuario
 });
 
 // Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-    console.log('Mensaje privado:', mensaje);
+    console.log('Mensaje Privado:', mensaje);
 });
